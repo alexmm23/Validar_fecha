09749cce63dfb83c9736c717f03152a954d9f647
alexmm23 <113723766+alexmm23@users.noreply.github.com>
2022-09-24 08:55:52 -0500

    Add files via upload
    Eliminé varias lineas de codigo innecesarias

--- ValidarFecha/ValidarFecha/ValidarFecha.cpp
+++ ValidarFecha/ValidarFecha/ValidarFecha.cpp
@@ -26,92 +26,64 @@ public:
 	}
 
 	Date(int, int, int);
-
-	void set_year(int);
-	void set_month(int);
-	void set_day(int);
 	int get_year(void) {return year; };
 	int get_month(void) { return month; };
 	int get_day(void) { return day; };
-
-	void check_date(void) { //Me base en la pagina https://www.abrirllave.com/c/ejercicio-validar-fecha.php#:~:text=Nota%201%3A%20Para%20que%20una,a%C3%B1o%20introducidos%20por%20el%20usuario.
-		
-		if (get_month() >= 1 && get_month() <= 12) 
-		{
-			switch (get_month()) {
-			case  1:
-			case  3:
-			case  5:
-			case  7:
-			case  8:
-			case 10:
-			case 12: if (get_day() < 1 || get_day() > 31 || get_year() < 1900 || get_year() > 2100)
-				valid_date = 0;
-				   else valid_date = 1;
-				break;
-
-			case  4:
-			case  6:
-			case  9:
-			case 11: if (get_day() < 1 || get_day() > 30 || get_year() < 1900 || get_year() > 2100)
-				valid_date = 0;
-				   else valid_date = 1;
-				break;
-
-			case  2: if (get_year() % 4 == 0 && get_year() % 100 != 0 || get_year() % 400 == 0)
-			{
-				if (get_day() >= 1 && get_day() <= 29)
-					valid_date = 1;
-			}
-				   else
-				if (get_day() >= 1 && get_day() <= 28)
-					valid_date = 1;
-			}
-		}
-
-		if (valid_date == 0) {
-			cout << "Fecha incorrecta" << endl;
-		}
-		else
-		{
-			cout << "Fecha correcta" << endl;
-		}
-	}
-
+	void check_date(void);//Me base en la pagina https://www.abrirllave.com/c/ejercicio-validar-fecha.php#:~:text=Nota%201%3A%20Para%20que%20una,a%C3%B1o%20introducidos%20por%20el%20usuario.
 
 };
 
-void Date::set_day(int _day) {
-	if (_day < 0 || day > 31) {
-		valid_date = 0;
-	}
-	else {
-		day = _day;
-	}
+//Constructor de la clase
+Date::Date(int INPUT_YEAR, int INPUT_MONTH, int INPUT_DAY) {
+	month = INPUT_MONTH;
+	day = INPUT_DAY;
 }
 
-void Date::set_month(int _month) {
-	if (_month < 0 || month > 12) {
-		valid_date = 0;
-	}
-	else {
-		month = _month;
-	}
-}
+//Definicion del metodo validar fecha
+void Date::check_date(void) {
+	if (get_month() >= 1 && get_month() <= 12)
+	{
+		switch (get_month()) {
+		case  1:
+		case  3:
+		case  5:
+		case  7:
+		case  8:
+		case 10:
+		case 12: if (get_day() < 1 || get_day() > 31 || get_year() < 1900 || get_year() > 2100)
+			valid_date = 0;
+			   else valid_date = 1;
+			break;
 
-void Date::set_year(int _year) {
-	if (_year < 0 || year > 3000) {
-		valid_date = 0;
-	}
-	else {
-		year = _year;
-	}
-}
+		case  4:
+		case  6:
+		case  9:
+		case 11: if (get_day() < 1 || get_day() > 30 || get_year() < 1900 || get_year() > 2100)
+			valid_date = 0;
+			   else valid_date = 1;
+			break;
 
-Date::Date(int INPUT_YEAR, int INPUT_MONTH, int INPUT_DAY) { // Aun no entiendo como usar el constructor
+		case  2: if (get_year() % 4 == 0 && get_year() % 100 != 0 || get_year() % 400 == 0)
+		{
+			if (get_day() >= 1 && get_day() <= 29)
+				valid_date = 1;
+		}
 	
-}
+			   else
+			if (get_day() >= 1 && get_day() <= 28)
+				valid_date = 1;
+		}
+	}
 
+	if (valid_date == 0) {
+		cout << "Fecha incorrecta" << endl;
+	}
+	else
+	{
+		cout << "Fecha correcta" << endl;
+	}
+}
+//Funcion para separar un string y convertirlo a int
 void split_str(string _fulldate) {
 	int nyear, nmonth, nday;
 	string word = "";
@@ -120,7 +92,7 @@ void split_str(string _fulldate) {
 	string array[Tamvec] = {};
 	int date_numbers[Tamvec] = {};
 	
-	_fulldate += '-';
+	_fulldate += '-'; //le añado un guion al final para poder cumplir el bucle
 	// Me base en el video de https://www.youtube.com/watch?v=BUy-SE6ZWGM&ab_channel=GeeksforGeeks
 	for (auto x : _fulldate)
 	{
@@ -141,18 +113,10 @@ void split_str(string _fulldate) {
 	nmonth = date_numbers[2];
 	nyear = date_numbers[3];	
 	
-	Date d1(nday, nmonth, nyear);
-	
-	d1.set_day(nday);
-	d1.set_month(nmonth);
-	d1.set_year(nyear);
+	Date d1(nyear, nmonth, nday);
 	d1.check_date();
 	cout << d1;
-	
-	
-
 } 
-
 
 int main()
 {
